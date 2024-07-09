5ca70c12ffad51c4d4974e64460f87e3ec395ce7
Predrag Đokić <predragdj@terieng.co.rs>
2024-07-09 23:38:58 +0200

    Init project.

--- src/main/java/rs/ac/bg/fon/mas/config/ConfigApplication.java
+++ src/main/java/rs/ac/bg/fon/mas/config/ConfigApplication.java
@@ -2,7 +2,9 @@ package rs.ac.bg.fon.mas.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
