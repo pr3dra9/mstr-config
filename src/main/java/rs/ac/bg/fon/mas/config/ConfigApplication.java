59825472e0e6828b52c85e44554fa7262dccadb8
Predrag Đokić <predragdj@terieng.co.rs>
2024-07-09 11:12:00 +0200

    Init project.

---
+++ src/main/java/rs/ac/bg/fon/mas/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.mas.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/mas/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.mas.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
