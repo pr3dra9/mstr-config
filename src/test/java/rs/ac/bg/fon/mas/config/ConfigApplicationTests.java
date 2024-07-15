f2100536317903448bc4d9cf0d1b4c3bc50244a6
Predrag Đokić <predragdj@terieng.co.rs>
2024-07-15 14:10:01 +0200

    Edited configs, enbled Discovery Service.

--- src/test/java/rs/ac/bg/fon/mas/config/ConfigApplicationTests.java
+++ src/test/java/rs/ac/bg/fon/mas/config/ConfigApplicationTests.java
@@ -3,7 +3,7 @@ package rs.ac.bg.fon.mas.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
 class ConfigApplicationTests {
 
 	@Test
